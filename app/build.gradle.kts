a2431d75cf8774e6ccacb305a0998760dc6f7bcd
IamMadhanMohan <mrmadhan09@gmail.com>
2025-02-21 16:42:24 -0500

    updated project with dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    // hilt dependencies
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -26,6 +29,12 @@ android {
             )
         }
     }
+
+    //    enables view_binding
+    buildFeatures{
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -45,4 +54,28 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Navigation Dependencies
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //Retrofit
+    implementation(libs.squareup.retrofit)
+    implementation(libs.squareup.convertergson)
+
+    //Viewmodel
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    //Glide
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.15.1")
+
+    //Hilt Dependencies
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+}
+
+// hilt dependency
+kapt {
+    correctErrorTypes = true
 }
