d936f7c801fbde12d1e86a75e3a197510e0b7271
IamMadhanMohan <mrmadhan09@gmail.com>
2025-02-24 15:34:55 -0500

    merged all features

--- app/src/main/java/com/pineneedle/pokemonapp/MainActivity.kt
+++ app/src/main/java/com/pineneedle/pokemonapp/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.pineneedle.pokemonapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
